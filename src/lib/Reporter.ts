21a728ed73547e66460310353e4173752f0a3964
javierbrea <javier.brea@gmail.com>
2025-02-12 14:18:12 +0100

    test: Fix E2E tests

--- src/lib/Reporter.ts
+++ src/lib/Reporter.ts
@@ -221,7 +221,7 @@ function errorReport(
   switch (reporter) {
     case "json":
       return JSON.stringify({
-        message: `${textTitle}. Found ${result.allowed.length} allowed, ${result.forbidden.length} forbidden and ${result.warning.length} dangerous licenses.`,
+        message: `${textTitle}. Found ${result.allowed.length} allowed, ${result.forbidden.length} forbidden and ${result.warning.length} dependencies.`,
         ...result,
       });
     case "markdown":
