a47d8888c32eec1e55df94c4720bf074b65e4772
javierbrea <javier.brea@gmail.com>
2025-02-12 14:29:47 +0100

    chore: Add license headers

--- src/lib/Paths.ts
+++ src/lib/Paths.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Telefónica Innovación Digital and contributors
+// SPDX-License-Identifier: Apache-2.0
+
 import path from "node:path";
 
 export const ROOT_PATH = path.resolve(import.meta.dirname, "..", "..");
--- src/lib/dependencies-reader/BaseSystemDependenciesReader.ts
+++ src/lib/dependencies-reader/BaseSystemDependenciesReader.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Telefónica Innovación Digital and contributors
+// SPDX-License-Identifier: Apache-2.0
+
 import globule from "globule";
 
 import { ROOT_PATH } from "../Paths.js";
